231105772bc98d3c08ba03b28c1b03bfa2835415
Jesen Tanadi <mail@jesentanadi.com>
2020-03-07 17:17:35 -0500

    fetch data from API endpoint instead

--- script.js
+++ script.js
@@ -1,63 +1,28 @@
-// import { inddScripts } from "./manifest"
+const body = document.querySelector("body");
 
-// This is here for now until we configure webpack
-const inddScripts = [
-  {
-    name: "Place Consecutive PDFs",
-    description: "Place PDFs from a folder into consecutive pages.",
-    path: "placeConsecutivePDFs/placeConsecutivePDFs.js"
-  },
-  {
-    name: "Batch Convert", 
-    description: "Convert a batch of files. Not written by RAA",
-    path: "batchConvert/batch_convert.jsxbin"
-  },
-  {
-    name: "Batch Relink",
-    description: "Relink images in a batch of folders",
-    path: "batchRelink/batchRelink.js"
-  },
-  {
-    name: "Batch Resize",
-    description: "Resize a batch of documents",
-    path: "batchResize/batchResize.js"
-  },
-  {
-    name: "Farrow & Ball",
-    description: "Blend between two gradients",
-    path: "farrow-n-ball/farrow-n-ball.js"
-  },
-  {
-    name: "Rename Layer",
-    description: "Rename layer in a batch of files",
-    path: "renameLayer/renameLayer.js"
-  },
-  {
-    name: "Check Images",
-    description: "Check image resolution in a batch of files",
-    path: "checkImages/checkImages.js"
-  },
-  {
-    name: "Gridnik",
-    description: "Place images in a grid (wip, kind of janky)",
-    path: "gridnik/gridnik.js"
-  }
-].sort((a, b) => {
-  if (a.name < b.name) return -1
-  else if (a.name > b.name) return 1
-  return 0
-})
+const inddH2 = document.createElement("h2");
+inddH2.innerText = "InDesign Scripts";
+body.appendChild(inddH2);
 
-const body = document.querySelector("body")
+fetch("https://raa-operator.herokuapp.com/api/getrepos/indd")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+    Object.entries(data).forEach(([dir, filenames]) => {
+      const dirUL = document.createElement("ul");
+      const dirLI = document.createElement("li");
+      const filesUL = document.createElement("ul");
 
-const inddH2 = document.createElement("h2")
-inddH2.innerText = "InDesign Scripts"
-body.appendChild(inddH2)
+      dirLI.innerHTML = dir;
 
-const inddUL = document.createElement("ul")
-inddScripts.forEach(script => {
-  const li = document.createElement("li")
-  li.innerHTML = `<h4 style="margin-bottom: 0;"><a href="https://cdn.jsdelivr.net/gh/raa-scripts/indd/${script.path}" download>${script.name}</a></h4><p style="margin-top: 0;">${script.description}</p>`
-  inddUL.appendChild(li)
-})
-body.appendChild(inddUL)
+      dirUL.appendChild(dirLI);
+      dirLI.appendChild(filesUL);
+
+      filenames.forEach(filename => {
+        const fileLI = document.createElement("li");
+        fileLI.innerHTML = `<h4 style="margin-bottom: 0;"><a href="https://cdn.jsdelivr.net/gh/raa-scripts/indd/${dir}/${filename}" download>${filename}</a></h4><p style="margin-top: 0;">${filename} description</p>`;
+        filesUL.appendChild(fileLI);
+      });
+      body.appendChild(dirUL);
+    });
+  });
