f030a2ea1e722e9dd2f673b6d7fcafcdcbd86506
Jesen Tanadi <mail@jesentanadi.com>
2020-03-12 07:59:06 -0400

    revert to jsdeliver

--- src/utils/getCDN.ts
+++ src/utils/getCDN.ts
@@ -1,4 +1,3 @@
 export default (repo: string, dir: string, fileName: string): string => {
-  return `https://raw.githubusercontent.com/raa-tools/${repo}/master/${dir}/${fileName}`
-  // return `https://cdn.jsdelivr.net/gh/raa-tools/${repo}/${dir}/${fileName}`
+  return `https://cdn.jsdelivr.net/gh/raa-tools/${repo}/${dir}/${fileName}`
 }
