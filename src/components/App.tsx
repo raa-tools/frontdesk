efb07d88f8d36f548f2331c1a000fabf68972f4a
Jesen Tanadi <mail@jesentanadi.com>
2020-03-15 22:55:32 -0400

    add selection & download functionalities

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, ReactElement } from "react"
+import React, { useState, useEffect, ReactElement, ChangeEvent } from "react"
 
 import repoSources from "../utils/repoSources"
+import { CheckedList } from "../types"
 
-import Repo from "./Repo"
+import DownloadButton from "./DownloadButton"
 import Loading from "./Loading"
+import Repo from "./Repo"
 
 const App: React.FC<{}> = (): ReactElement => {
   const [repos, setRepos] = useState([])
-  const [loading, setLoading] = useState(true)
-
   useEffect((): void => {
     const loadRepos = async (): Promise<void> => {
       for (const source of repoSources) {
@@ -19,25 +19,48 @@ const App: React.FC<{}> = (): ReactElement => {
           })
         } catch (e) {
           console.error(
-            `Something went wrong while fetching ${source.name} at ${source.url}: ${e}`
+            `Something went wrong while fetching ${source.name} at ${source.url}\n${e}`
           )
         }
       }
-      setLoading(false)
     }
     loadRepos()
   }, [])
 
-  if (!repos.length) return null
+  const [downloadPaths, setDownloadPaths] = useState<CheckedList>({})
+  const handleSelect = (e: ChangeEvent<HTMLInputElement>): void => {
+    // const downloadTarget: DownloadTarget = JSON.parse(e.target.id)
+    const { id } = e.target
+
+    if (e.target.checked) {
+      setDownloadPaths(prev => ({ ...prev, [id]: true }))
+    } else {
+      setDownloadPaths(prev => ({ ...prev, [id]: false }))
+    }
+  }
+
+  const handleDownload = (): void => {
+    // clear selects
+    setDownloadPaths({})
+  }
 
   return (
     <>
-      {loading ? (
+      {!repos.length ? (
         <Loading />
       ) : (
-        repos.map((repo, i) => (
-          <Repo key={i} repoName={repo.name} dirs={repo.dirs} />
-        ))
+        <div>
+          <DownloadButton paths={downloadPaths} handleClick={handleDownload} />
+          {repos.map((repo, i) => (
+            <Repo
+              key={i}
+              repoName={repo.name}
+              dirs={repo.dirs}
+              handleSelect={handleSelect}
+              checkedItems={downloadPaths}
+            />
+          ))}
+        </div>
       )}
     </>
   )
